9a8e0d164e3ae576f8a2589907db8e26c63788d0
John Creeden <johncreeden@Johns-MBP.home>
2017-06-28 01:44:31 -0400

    first commit

---
+++ footer.php
@@ -0,0 +1,6 @@
+</div> <!-- END PAGE CONTAINER -->
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script src="<?php bloginfo('template_directory') ?>/assets/js/main.min.js"></script>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta http-equiv="x-ua-compatible" content="IE=Edge"/>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
+	<title><?= get_bloginfo( 'name' ) . ' | ' . get_the_title() ?></title>
+	<link rel="stylesheet" href="<?php echo bloginfo('template_url') . '/assets/styles/elusive-icons.min.css'; ?>">
+	<link href="https://fonts.googleapis.com/css?family=Droid+Serif|Raleway" rel="stylesheet">
+	<link rel="stylesheet" href="<?php echo bloginfo('template_url') . '/assets/styles/main.css'; ?>">
+</head>
+
+<body class="<?php echo $pagename; ?>">
+
+<div class="header">
+	<div class="container">
+		<div class="logo">
+		<img src="<?php bloginfo('template_directory') ?>/assets/images/logo_rhr.png">
+		</div>
+		<nav>
+			<div class="nav__links">
+				<ul>
+					<li><a href="#" class="active">Home</a></li>
+					<li><a href="#">Your Business</a></li>
+					<li><a href="#">How We Can Help</a></li>
+					<li><a href="#">Resources</a></li>
+				</ul>
+			</div>
+			
+			<div class="nav__icons__search">
+				<ul>
+					<li><a href="#"><i class="el el-search"></i></a></li>
+					<li><a href="#"><i class="el el-comment"></i></a></li>
+				</ul>
+			</div>
+			<div class="nav__icons__media">
+				<ul>
+					<li><a href="#"><i class="el el-linkedin"></i></a></li>
+					<li><a href="#"><i class="el el-book"></i></a></li>
+					<li><a href="#"><i class="el el-mic"></i></a></li>
+					<li><a href="#"><i class="el el-wordpress"></i></a></li>
+				</ul>
+			</div>
+			
+		</nav>
+	</div>
+</div>
+
+<!-- BEGIN PAGE CONTAINER -->
+<div class="page_container">
